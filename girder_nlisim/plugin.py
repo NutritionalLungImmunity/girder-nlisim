261382a1b052dd8b6e240a8a0e5c4fc52cbb061c
Jonathan Beezley <jonathan.beezley@kitware.com>
2020-12-02 15:51:57 -0500

    Add job execution progress to the simulation model

--- girder_nlisim/plugin.py
+++ girder_nlisim/plugin.py
@@ -1,6 +1,27 @@
+from girder import events, logger
 from girder.plugin import getPlugin, GirderPlugin
 
-from girder_nlisim.api import NLI
+from girder_nlisim.api import NLI, NLI_JOB_TYPE
+from girder_nlisim.models import Simulation
+
+
+def update_status(event):
+    simulation_model = Simulation()
+    job = event.info['job']
+    if job['type'] != NLI_JOB_TYPE:
+        return
+
+    simulation_id = job['kwargs'].get('simulation_id')
+    simulation = simulation_model.load(simulation_id, force=True)
+
+    if simulation is None:
+        logger.error(f'Could not find simulation for job {job["_id"]}')
+        return
+
+    progress = job['progress']
+    simulation['nli']['progress'] = 100 * (progress['current'] / progress['total'])
+    simulation['nli']['status'] = job['status']
+    simulation_model.save(simulation)
 
 
 class NLIGirderPlugin(GirderPlugin):
@@ -9,3 +30,5 @@ class NLIGirderPlugin(GirderPlugin):
     def load(self, info):
         getPlugin('jobs').load(info)
         info['apiRoot'].nli = NLI()
+
+        events.bind('jobs.job.update.after', 'nlisim', update_status)
