356209d9cef32e4d648c7633345359ea717c95ca
Jonathan Beezley <jonathan.beezley@kitware.com>
2020-12-31 13:50:42 -0500

    Remove geometry download from task
    As of the most recent version of nlisim, the geometry file is included
    in the python package.

--- girder_nlisim/tasks.py
+++ girder_nlisim/tasks.py
@@ -2,7 +2,6 @@ from logging import getLogger
 from pathlib import Path
 from tempfile import NamedTemporaryFile, TemporaryDirectory
 from typing import Any, Dict
-from urllib.request import urlopen
 
 import attr
 from celery import Task
@@ -15,9 +14,6 @@ from nlisim.postprocess import generate_vtk
 from nlisim.solver import run_iterator, Status
 
 logger = getLogger(__name__)
-GEOMETRY_FILE_URL = (
-    'https://data.nutritionallungimmunity.org/api/v1/file/5ebd86cec1b2cfe0661e681f/download'
-)
 
 
 @attr.s(auto_attribs=True, kw_only=True)
@@ -77,14 +73,6 @@ class GirderConfig:
         return folder
 
 
-def download_geometry():
-    geometry_file_path = Path('geometry.hdf5')
-    if not geometry_file_path.is_file():
-        with urlopen(GEOMETRY_FILE_URL) as f, geometry_file_path.open('wb') as g:
-            # TODO: stream into file if it becomes large
-            g.write(f.read())
-
-
 @app.task(bind=True)
 def run_simulation(
     self: Task,
@@ -105,7 +93,6 @@ def run_simulation(
         )
         girder_config.set_status(job['_id'], JobStatus.RUNNING, current_time, target_time)
 
-        download_geometry()
         time_step = 0
 
         for state, status in run_iterator(simulation_config, target_time):
