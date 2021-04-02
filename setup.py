a2810fc6a8d675f5a6c1614f49c7a7f47e8cab11
Jonathan Beezley <jonathan.beezley@kitware.com>
2021-04-02 07:21:15 -0600

    Increment version number

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     name='girder_nlisim',
     packages=find_packages(exclude=['test', 'test.*']),
     url='https://nutritionallungimmunity.org/',
-    version='0.3.3',
+    version='0.3.4',
     zip_safe=False,
     entry_points={'girder.plugin': ['nli = girder_nlisim.plugin:NLIGirderPlugin']},
 )
