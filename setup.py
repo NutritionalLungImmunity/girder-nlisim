b155c8268acdbad6998bf6f957a3e16e1660a056
Adam Knapp <adam.knapp@instanton.org>
2021-04-28 11:35:41 -0400

    put the requirements in separate file

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import find_packages, setup
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-requirements = ['celery<5', 'girder>=3.0.0', 'girder-client>=3.0.0', 'girder-jobs', 'nlisim>=0.2.9']
+with open('requirements.txt', 'r') as file:
+    requirements = file.read().splitlines()
 
 setup(
     author='Nutritional Lung Immunity',
